639f67f95167ee36f238d70afb5cbec7d62e860a
Mohammed-mehdi-raza <mehdirazajafri2040@gmail.com>
2022-09-27 16:34:38 +0530

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".typed2", {
+    strings: [
+        "Web devloper",
+        "Programmer",
+        "Student"
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
